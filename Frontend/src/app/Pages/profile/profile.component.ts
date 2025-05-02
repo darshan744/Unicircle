e6010c42b311853b85d9808a4849521255f09fe2
Darshan <darshankumaravelk@gmail.com>
2025-05-02 22:12:29 +0530

    Minor style changes

--- Frontend/src/app/Pages/profile/profile.component.ts
+++ Frontend/src/app/Pages/profile/profile.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import {Tab, TabList, Tabs} from 'primeng/tabs'
+import {Avatar} from 'primeng/avatar'
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [Avatar, Tabs, TabList, Tab],
+  // styles: '.p-tablist-tab-list {gap:10rem !important;}',
+  styleUrl: './profile.component.css',
   templateUrl: './profile.component.html',
 })
 export class ProfileComponent {
-
+  userNameEditable: boolean = true;
+  nameEditable: boolean = true;
+  passwordEditable: boolean = true;
 }
